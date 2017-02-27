be7e85ed1eebba894410a2908b262acc873914d2
PochiDcoder <prashanth.pochi@gmail.com>
2017-02-27 00:54:08 -0500

    Files related to mongodb and spring

---
+++ AccessingDataMongoDB/src/main/java/hello/Customer.java
@@ -0,0 +1,28 @@
+package hello;
+
+/**
+ * Created by prashanth pochiraju on 2/26/2017.
+ */
+import org.springframework.data.annotation.Id;
+public class Customer {
+    @Id
+    public String id;
+
+    public String firstName;
+    public String lastName;
+
+    public Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
